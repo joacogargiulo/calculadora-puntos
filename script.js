519a20cf41840290b30dfd56226906497be978ce
joacogargiulo <joacogargiulo04@gmail.com>
2024-11-28 17:38:15 -0300

    Modularidad mejorada, promedios agregados

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-let tablaActual
-let tablaAnual
-let partidosRestantes
+const matchesContainer = document.getElementById("matches-container");
+const tbody = document.getElementById("tabla-puntos");
+const tbodyAnual = document.getElementById("tabla-anual");
+const tbodyPromedios = document.getElementById("tabla-promedios");
+const urlAPI = "https://api-promiedos.onrender.com"
 
-async function obtenerDatosTablas() {
-    try {
-        const response = await fetch('https://api-promiedos.onrender.com/posiciones'); 
-        const data = await response.json();
+let clasificados = []
+let cuposLibertadores = 3
+let cuposSudamericana = 6
+let tablaActual = []
+let tablaAnual = []
+let tablaPromedios = []
+let partidosRestantes = []
+let currentFecha = null;
 
-        // Asignar las tablas recibidas a las variables locales
-        tablaActual = data.tablaPuntosPrimera;
-        tablaAnual = data.tablaAnual;
-
-        calcularPuntos(tablaActual)
-        calcularPuntos(tablaAnual)
-        // Renderizar las tablas en la página
-        renderTabla();
-        renderTablaAnual();
-    } catch (error) {
-        console.error('Error al obtener los datos de la API:', error);
-    }
-}
-
-async function obtenerPartidos() {
-    try {
-        const response = await fetch('https://api-promiedos.onrender.com/partido')
-        const data = await response.json()
-
-        
-        filtrarPartidos(data)
-        
-    } catch (error) {
-        
-    }
-}
-
-
-// Lista completa de partidosRestantes restantes con fechas
 const partidos = [
     // Fecha 24
     { fecha: 24, equipo1: "Tigre", equipo2: "Instituto" },
@@ -99,18 +76,67 @@ const partidos = [
     { fecha: 27, equipo1: "Velez", equipo2: "Huracan" }
 ];
 
-// Seleccionar elementos del DOM para ambas tablas.
-const tbody = document.getElementById("tabla-puntos");
-const tbodyAnual = document.getElementById("tabla-anual");
+function manejarError(mensaje, error) {
+    console.error(`${mensaje}`, error);    
+}
+
+async function fetchDatos(url) {
+    try {
+        const response = await fetch(url)
+        return response.json()
+        
+    } catch (error) {
+        manejarError(`Error al obtener los datos de ${url}`, error)
+    }
+}
+
+async function obtenerDatosTablas() {
+    try {
+        const data = await fetchDatos(urlAPI+'/posiciones')
+
+        // Asignar las tablas recibidas a las variables locales
+        tablaActual = data.tablaPuntosPrimera;
+        tablaAnual = data.tablaAnual;
+        tablaPromedios = data.tablaPromedios
+        
+        clasificados = [
+            tablaAnual.find((e => e.equipo === "Estudiantes (LP)")),
+            tablaAnual.find((e => e.equipo === "Racing Club"))
+        ]
+        actualizarTablas()
+        obtenerPartidos()
+    } catch (error) {
+        manejarError('Error al obtener las tablas:', error);
+    }
+}
+
+
+async function obtenerPartidos() {
+    try {
+        const data = await fetchDatos(urlAPI+'/partido')        
+        filtrarPartidos(data)
+    } catch (error) {
+        manejarError('Error al obtener los partidos:', error);        
+    }
+}
+
+function actualizarTablas() {
+    actualizarPuntos(tablaActual)
+    actualizarPuntos(tablaAnual)
+    actualizarPuntos(tablaPromedios)
+    renderTabla(tbody, tablaActual)
+    renderTabla(tbodyAnual, tablaAnual)
+    renderTablaPromedios()
+}
+
 
 function filtrarPartidos(proxPartido) {    
-    indice = partidos.findIndex(obj => 
-            obj.equipo1 === proxPartido.equipo1 &&
-            obj.equipo2 === proxPartido.equipo2
-        );
+    const indice = partidos.findIndex(
+        (p) => p.equipo1 === proxPartido.equipo1 && p.equipo2 === proxPartido.equipo2
+    );
     if (indice === -1){
-        console.log("error filtrando");
-        return partidos;
+        manejarError('Error filtrando los partidos', proxPartido)
+        return
     }
         
     // Retorna los elementos desde el índice encontrado
@@ -118,54 +144,28 @@ function filtrarPartidos(proxPartido) {
     renderizarPartidos()
 }
 
-// Generar la tabla inicial de puntos.
-function renderTabla() {
-    tbody.innerHTML = ""; // Limpiar la tabla antes de actualizar
-    tablaActual.sort((a, b) => b.pts - a.pts || b.dif - a.dif); // Ordenar por puntos y diferencia de goles
-
-    tablaActual.forEach((equipo, index) => {
-        const row = document.createElement("tr");
-
-        const cells = [
-            index + 1,
-            equipo.equipo,
-            equipo.pts,
-            equipo.pj,
-            equipo.pg,
-            equipo.pe,
-            equipo.pp,
-            equipo.gf,
-            equipo.gc,
-            equipo.dif > 0 ? `+${equipo.dif}` : `${equipo.dif}`,
-        ];
-
-        cells.forEach(cellData => {
-            const cell = document.createElement("td");
-            cell.textContent = cellData;
-            row.appendChild(cell);
-        });
-
-        if (index === 0) {
-            row.classList.add("libertadores")
+function revisarCupos() {
+    cuposLibertadores = 3
+    cuposSudamericana = 6
+    clasificados.sort((a, b) => b.pts - a.pts || b.dif - a.dif)
+    clasificados.forEach((clasificado) => {
+        const posicion = tablaAnual.findIndex(e => e.equipo === clasificado.equipo)        
+        if (posicion < cuposLibertadores) {
+            cuposLibertadores++
+        } else if (posicion < (cuposSudamericana+cuposLibertadores)) {
+            cuposSudamericana++
         }
-
-        tbody.appendChild(row);
-    });
+    })
 }
-
-// Generar la tabla anual de puntos
-function renderTablaAnual() {
-    tbodyAnual.innerHTML = ""; // Limpiar la tabla antes de actualizar.
-    
+function renderTabla(container, tabla) {
+    container.innerHTML = ""; // Limpiar la tabla antes de actualizar.
     // Ordenar la tabla por puntos y diferencia de goles.
-    tablaAnual.sort((a, b) => b.pts - a.pts || b.dif - a.dif);
+    tabla.sort((a, b) => b.pts - a.pts || b.dif - a.dif);
 
-    // Verificar la posición de Estudiantes (LP).
-    let estudiantesPos = tablaAnual.findIndex(e => e.equipo === "Estudiantes (LP)");
-    let racingPos = tablaAnual.findIndex(e => e.equipo === "Racing Club");
-
+    if(tabla === tablaAnual) revisarCupos()
+    
     // Determinar clases de colores según posiciones.
-    tablaAnual.forEach((equipo, index) => {
+    tabla.forEach((equipo, index) => {
         const row = document.createElement("tr");
         const cells = [
             index + 1,
@@ -179,83 +179,113 @@ function renderTablaAnual() {
             equipo.gc,
             equipo.dif > 0 ? `+${equipo.dif}` : `${equipo.dif}`,
         ];
-
+        
         cells.forEach((cellData, cellIndex) => {
             const cell = document.createElement("td");
             cell.textContent = cellData;
             row.appendChild(cell);
 
-            // Aplicar colores según la clasificación.
-            if (cellIndex === 0) {
-                if (equipo.equipo === "Estudiantes (LP)" || equipo.equipo === "Racing Club") {
-                    cell.classList.add("estudiantes");
-                } else if (
-                    index < 3 ||
-                    (estudiantesPos >= 0 && estudiantesPos < 3 && index < 4) ||
-                    (racingPos >= 0 && racingPos < 3 && index < 4) ||
-                    (racingPos >= 0 && racingPos < 3 && estudiantesPos >= 0 && estudiantesPos < 3 && index < 5)
-                ) {
-                    cell.classList.add("libertadores");
-                } else if (
-                    index < 9 ||
-                    (estudiantesPos >= 0 && estudiantesPos <= 8 && index < 10) ||
-                    (racingPos >= 0 && racingPos < 8 && index < 10) ||
-                    (racingPos >= 0 && racingPos < 8 && estudiantesPos >= 0 && estudiantesPos < 8 && index < 11)
-                ) {
-                    cell.classList.add("sudamericana");
+            if (cellIndex === 1) {
+                const imagen = document.createElement("img")
+                imagen.src = `https://promiedos.com.ar/${equipo.escudo}`
+                cell.prepend(imagen)
+            }
+            
+            if (tabla === tablaAnual) {
+                // Aplicar colores según la clasificación.
+                if (cellIndex === 0) {
+                    if (equipo.equipo === "Estudiantes (LP)" || equipo.equipo === "Racing Club") {
+                        cell.classList.add("clasificado");
+                    } else if (
+                        index < cuposLibertadores
+                    ) {
+                        cell.classList.add("libertadores");
+                    } else if (
+                        index < (cuposLibertadores+cuposSudamericana)
+                    ) {
+                        cell.classList.add("sudamericana");
+                    }
                 }
-            } 
+            } else {
+                index === 0 ? row.classList.add('libertadores') : null
+            }
         });
-        tbodyAnual.appendChild(row);
+        container.appendChild(row);
     });
 }
 
-// Calcular puntos y actualizar ambas tablas.
-function calcularPuntos(tabla) {
-    // Calcular nuevos puntos y estadísticas en base a los resultados ingresados.
+function renderTablaPromedios() {
+    tbodyPromedios.innerHTML = ""; // Limpiar la tabla antes de actualizar.
+    // Ordenar la tabla por puntos y diferencia de goles.
+    tablaPromedios.sort((a, b) => b.pts - a.pts || b.dif - a.dif);
+    
+    tablaPromedios.forEach((equipo, index) => {
+        const row = document.createElement("tr");
+        const cells = [
+            index + 1,
+            equipo.equipo,
+            equipo.temporada22,
+            equipo.temporada23,
+            equipo.temporada24,
+            equipo.pts,
+            equipo.pj,
+            equipo.prom.toFixed(3),
+        ];
+        
+        cells.forEach((cellData, cellIndex) => {
+            const cell = document.createElement("td");
+            cell.textContent = cellData;
+            row.appendChild(cell);
+
+            if (cellIndex === 1) {
+                const imagen = document.createElement("img")
+                imagen.src = `https://promiedos.com.ar/${equipo.escudo}`
+                cell.prepend(imagen)
+            }
+
+        });
+
+        tbodyPromedios.appendChild(row);
+    });
+
+}
+
+function actualizarPuntos(tabla) {
     partidosRestantes.forEach((partido, index) => {
         const score1 = parseInt(document.getElementById(`score-${index}-team1`).value)
         const score2 = parseInt(document.getElementById(`score-${index}-team2`).value)
         
         if (!isNaN(score1) && !isNaN(score2)) {
-            
             const equipo1 = tabla.find(e => e.equipo === partido.equipo1);
-            const equipo2 = tabla.find(e => e.equipo === partido.equipo2);
-    
-            equipo1.pj += 1;
-            equipo2.pj += 1;
-            equipo1.gf += score1;
-            equipo2.gf += score2;
-            equipo1.gc += score2;
-            equipo2.gc += score1;
-    
-            if (score1 > score2) {
-                equipo1.pts += 3;
-                equipo1.pg += 1;
-                equipo2.pp += 1;
-            } else if (score1 < score2) {
-                equipo2.pts += 3;
-                equipo2.pg += 1;
-                equipo1.pp += 1;
-            } else {
-                equipo1.pts += 1;
-                equipo2.pts += 1;
-                equipo1.pe += 1;
-                equipo2.pe += 1;
-            }
-    
-            equipo1.dif = equipo1.gf - equipo1.gc;
-            equipo2.dif = equipo2.gf - equipo2.gc;
+            const equipo2 = tabla.find(e => e.equipo === partido.equipo2); 
+
+            calcularPuntos(equipo1, score1, score2)
+            calcularPuntos(equipo2, score2, score1)
         }
     });
-        
+}
 
+function calcularPuntos(equipo, golesFavor, golesContra) {
+    equipo.pj ++
+    equipo.gf += golesFavor
+    equipo.gc += golesContra
+
+    if (golesFavor > golesContra) {
+        equipo.pts += 3
+        equipo.temporada24 +=3
+        equipo.pg ++
+    } else if (golesFavor < golesContra) {
+        equipo.pp ++
+    } else {
+        equipo.pts ++
+        equipo.pe ++
+    }
+
+    equipo.prom = equipo.pts / equipo.pj
+    equipo.dif = equipo.gf - equipo.gc
 }
 
 // Generar los inputs para los partidosRestantes y dividir por fecha.
-const matchesContainer = document.getElementById("matches-container");
-let currentFecha = null;
-
 function renderizarPartidos() {
     partidosRestantes.forEach((partido, index) => {    
         // Crear un divisor de fecha si cambia la fecha.
@@ -271,16 +301,24 @@ function renderizarPartidos() {
         matchDiv.classList.add("match");
         
         matchDiv.innerHTML = `
-        <label>${partido.equipo1} vs ${partido.equipo2}</label>
-        <input type="number" id="score-${index}-team1" min=0>
-        <span> - </span>
-        <input type="number" id="score-${index}-team2" min="0">
+        <div class="match-team">
+            <img src="https://promiedos.com.ar/${tablaActual.find((e) => e.equipo === partido.equipo1).escudo}" class="team-logo">
+            <span>${partido.equipo1}</span>
+        </div>
+        <div class="match-score">
+            <input type="number" id="score-${index}-team1" min=0>
+            <span> - </span>
+            <input type="number" id="score-${index}-team2" min="0">
+        </div>
+        <div class="match-team">
+            <span>${partido.equipo2}</span>
+            <img src="https://promiedos.com.ar/${tablaActual.find((e) => e.equipo === partido.equipo2).escudo}" class="team-logo">
+        </div>
         `;
         matchesContainer.appendChild(matchDiv);
     });
-    obtenerDatosTablas()
+    // obtenerDatosTablas()
 }
-
 
 // Función para generar resultados aleatorios entre 0 y 3 para cada input
 function resultadosRandom() {
@@ -295,4 +333,5 @@ function resultadosRandom() {
 
 matchesContainer.addEventListener("input", obtenerDatosTablas)
 
-obtenerPartidos()
+obtenerDatosTablas()
+// obtenerPartidos()
